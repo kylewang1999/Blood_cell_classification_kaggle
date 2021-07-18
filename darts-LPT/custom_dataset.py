22919826e2cd826a18da9691f35094103ed51b25
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-07-18 21:51:27 +0800

    sync

--- darts-LPT/custom_dataset.py
+++ darts-LPT/custom_dataset.py
@@ -171,16 +171,25 @@ def preprocess_data(train_df, valid_df, batch_size, train_search=False):
         for step, data in enumerate(train_queue):
             input = data['image']
             target = data['label']
+            if step == 10:
+                print(input.size)
+                print(target.numpy())
             if np.any(np.isnan(input.numpy())) or np.any(np.isinf(input.numpy())) or np.any(np.isnan(target.numpy())):
                 print("SOMETHING IS WRONG WITH TRAIN QUEUE")
         for step, data in enumerate(valid_queue):
             input = data['image']
             target = data['label']
+            if step == 10:
+                print(input.size)
+                print(target.numpy())
             if np.any(np.isnan(input.numpy())) or np.any(np.isinf(input.numpy())) or np.any(np.isnan(target.numpy())):
                 print("SOMETHING IS WRONG WITH VALID QUEUE")
         for step, data in enumerate(external_queue):
             input = data['image']
             target = data['label']
+            if step == 10:
+                print(input.size)
+                print(target.numpy())
             if np.any(np.isnan(input.numpy())) or np.any(np.isinf(input.numpy())) or np.any(np.isnan(target.numpy())):
                 print("SOMETHING IS WRONG WITH EXTERNAL QUEUE")
             # print(target.numpy)
