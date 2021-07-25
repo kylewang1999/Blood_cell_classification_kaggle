91312668a9ec321e59e3ba53f36c003b1bfdcb39
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-07-25 16:32:02 +0800

    sync

--- cluster/bash/gpu_debug.sh
+++ cluster/bash/gpu_debug.sh
@@ -1,4 +1,5 @@
 # Setting up working dir
+echo Start Copying
 SECONDS=0           # Tracking Time Elapsed
 cp -r /k5wang-volume/Blood_cell_classification_kaggle/kaggle /local/kaggle
 # cd /local/Blood_cell_classification_kaggle/darts-LPT/
@@ -10,7 +11,7 @@ echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 
 
 # Single GPU
-python train_search_ts.py --batch_size 3 --epochs 25 --save 50-LR_0.005-FIXED_LOSS 
+python train_search_ts.py --batch_size 3 --epochs 25 --save GPU1080
 
 # Parallel Processing: 2 GPU
 # python ../darts-LPT/train_search_ts.py --unrolled --batch_size 3 --epochs 25 --save 50-LR_0.005-FIXED_LOSS --is_parallel 1, --gpu 0,1
