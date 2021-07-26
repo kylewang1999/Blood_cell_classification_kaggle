85dc2cfa030e9de6481adeee44daf993c019eb5f
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-07-26 22:19:36 +0800

    sync

--- cluster/bash/train.sh
+++ cluster/bash/train.sh
@@ -1,14 +1,11 @@
 # Setting up working dir
 echo Start Copying
-SECONDS=0           # Tracking Time Elapsed
+SECONDS=0
 cp -r /k5wang-volume/Blood_cell_classification_kaggle/kaggle /local/kaggle
-# cd /local/Blood_cell_classification_kaggle/darts-LPT/
 cd /k5wang-volume/Blood_cell_classification_kaggle/darts-LPT
 pwd
-echo Copying DONE. 
-duration=$SECONDS   # Displaying Time Elapsed
+echo Copying DONE.
+duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 
-# Single GPU
 python train_custom_local.py --batch_size 3 --arch DARTS_TS_BC_25EPOCH --epochs 50 --save SEARCH_TS_WAUX
-
