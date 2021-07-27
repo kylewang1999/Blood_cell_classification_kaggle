25053bd6e68ce7384efb3c044f68746ff499b510
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-07-27 15:01:42 +0800

    sync

--- cluster/bash/train.sh
+++ cluster/bash/train.sh
@@ -8,6 +8,6 @@ echo Copying DONE.
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 
-python train_custom.py --batch_size 3 --arch DARTS_TS_BC_25EPOCH --epochs 50 --save SEARCH_TS_WAUX
+python train_custom.py --batch_size 3 --arch DARTS_TS_BC_25EPOCH --epochs 50 --save SEARCH_TS_WAUX_LR0.005 --learning_rate 0.005
 
 #python train_custom_colab.py --batch_size 20 --epochs 50 --save FOO --dataset_path ../kaggle/blood_cell/
