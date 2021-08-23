6444b1c2b933856e6dabf8ac94c46fe541ddbd3a
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-08-23 17:52:28 +0800

    sync

---
+++ cluster/bash/test_Reorg.sh
@@ -0,0 +1,48 @@
+cd /k5wang-volume/Blood_cell_classification_kaggle/darts-LPT
+
+# Testing Blood_cell searched model
+python test.py --model_path ./eval-Blood_cell-50Epoch-WAUX-LR0.005/weights.pt --arch DARTS_TS_BC_25EPOCH --batch_size 3
+
+# Testing CIFAR searched model
+# python test.py --model_path ./eval-CIFAR-50Epoch-WAUX/weights.pt --arch DARTS_CIFAR10_TS_1ST
+
+# cf100-300
+# cd /k5wang-volume/Blood_cell_classification_kaggle/darts-LPT/eval-CF100-300-20210801-012406# pwd/k5wang-volume/Blood_cell_classification_kaggle/darts-LPT/eval-CF100-300-20210801-012406
+# python test.py --model_path ./eval-CF100-300-20210801-012406/weights.pt --arch DARTS_CIFAR100_TS_1ST --batch_size 8 --layers 12
+    # 08/02 10:31:50 AM test_acc 71.531966
+
+# cf10-300
+# cd /k5wang-volume/Blood_cell_classification_kaggle/darts-LPT/
+# python test.py --model_path ./eval-CF10-300-20210801-012406/weights.pt --arch DARTS_CIFAR10_TS_1ST --batch_size 8 --layers 12
+    # 08/03 05:27:47 AM test_acc 62.042622
+
+# BC-50-300
+# cd /k5wang-volume/Blood_cell_classification_kaggle/darts-LPT/
+# python test.py --model_path ./eval-BC-50-300-20210808-070655/weights.pt --arch DARTS_TS_BC_50EPOCH --batch_size 8 --layers 12
+
+    
+# BC-25-300
+# cd /k5wang-volume/Blood_cell_classification_kaggle/darts-LPT/
+# python test.py --model_path ./eval-BC-25-300-20210731-025530/weights.pt --arch DARTS_TS_BC_25EPOCH --batch_size 8 --layers 12 
+
+    # 08/04 05:36:11 AM test_acc 54.242059
+
+# python test.py --model_path ./eval-BC-50-300-20210808-070655/weights.pt --arch DARTS_TS_BC_50EPOCH --batch_size 8 --layers 12 
+
+# Official Darts 50 Epoch Search
+# python test.py --model_path ./eval-OFF-BC-50-300-20210818-014314/weights.pt --arch DARTS_BC_50EPOCH --batch_size 8 --layers 12 
+    # 6 Epoch
+    # 08/18 02:43:15 AM test_acc 86.288701
+    # 300 Epoch
+    # 08/19 01:53:04 PM test_acc 88.379574
+
+
+# Hybrid Darts-LPT
+# python test.py --model_path ./eval-darts-hybrid-20210819-135637/weights.pt --arch DARTS_TS_BC_50EPOCH --batch_size 8 --layers 12 
+    # 12 Epoch
+    # 08/19 03:47:17 PM test_acc 86.529956
+    # 300 Epoch (Improved Dataset)
+    # 08/21 01:42:30 PM test_acc 88.299156
+
+
+    
