96ec36dd34de6a4f771a4cffd04b69e6d9b7235b
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-08-23 17:53:07 +0800

    sync

--- cluster/bash/train-BC-50-300.sh
+++ cluster/bash/train-BC-50-300.sh
@@ -9,7 +9,7 @@ duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 
 # python train_custom.py --batch_size 3 --arch DARTS_TS_BC_25EPOCH --epochs 300 --save BC-300Epoch --learning_rate 0.005
-python train_custom.py --batch_size 8 --layers 12 --arch DARTS_TS_BC_50EPOCH --epochs 300 --save BC-50-300 --learning_rate 0.005 
+python train_custom.py --batch_size 8 --layers 12 --arch DARTS_TS_BC_50EPOCH --epochs 300 --save BC-50-300-Reorg --learning_rate 0.005 
 
 
 # python train_custom.py --batch_size 8 --layers 12 --arch DARTS_TS_BC_50EPOCH --epochs 300 --save FOO --learning_rate 0.005 --dataset_path ../kaggle/blood_cell/ | grep Experiment | cat
--- darts-LPT/train_custom.py
+++ darts-LPT/train_custom.py
@@ -45,7 +45,6 @@ parser.add_argument('--arch', type=str, default='DARTS_CIFAR10_TS_1ST', help='wh
 parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
 parser.add_argument('--resume', type=str, default='')
 parser.add_argument('--is_cifar100', type=int, default=0)
-# parser.add_argument('--dataset_path', type=str, default='/local/kaggle/blood_cell/', help='location of the data corpus')
 parser.add_argument('--dataset_path', type=str, default='/local/kaggle/BCCD_Reorganized/', help='location of the data corpus')
 args = parser.parse_args()
 
