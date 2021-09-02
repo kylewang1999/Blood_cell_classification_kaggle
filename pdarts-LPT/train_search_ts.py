761c74acf85b33a908779b7d67e9e9e774604f29
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-09-02 23:45:24 +0800

    sync

--- pdarts-LPT/train_search_ts.py
+++ pdarts-LPT/train_search_ts.py
@@ -26,7 +26,7 @@ parser.add_argument('--workers', type=int, default=2,
                     help='number of workers to load dataset')
 parser.add_argument('--batch_size', type=int, default=96, help='batch size')
 parser.add_argument('--learning_rate', type=float,
-                    default=0.025, help='init learning rate')
+                    default=0.005, help='init learning rate')
 parser.add_argument('--learning_rate_min', type=float,
                     default=0.0, help='min learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
