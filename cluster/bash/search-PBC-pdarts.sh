1c02236f370d8f7c4b0671b2843ed9f456452e96
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-09-03 00:09:52 +0800

    sync

--- cluster/bash/search-PBC-pdarts.sh
+++ cluster/bash/search-PBC-pdarts.sh
@@ -33,7 +33,7 @@ echo Copying DONE.
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 
-python train_search_ts.py --batch_size 4 --epochs 50 --note PBC-darts-50 
+python train_search_ts.py --batch_size 4 --epochs 50 --note PBC-darts-50 --local-mount 0
 # python train_search_ts.py --batch_size 4 --epochs 50 --save FOO --local_mount 0
 
 
