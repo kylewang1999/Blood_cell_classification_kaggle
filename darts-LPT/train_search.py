ced88e0b48207dd904b18427f65a8121d31ccebe
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-09-03 12:09:46 +0800

    sync

--- darts-LPT/train_search.py
+++ darts-LPT/train_search.py
@@ -22,7 +22,7 @@ import mendely_dataloader as loader
 parser = argparse.ArgumentParser("cifar")
 parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
 parser.add_argument('--batch_size', type=int, default=64, help='batch size')
-parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
+parser.add_argument('--learning_rate', type=float, default=0.005, help='init learning rate')
 parser.add_argument('--learning_rate_min', type=float, default=0.001, help='min learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
