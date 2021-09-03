654f4a8ff4d40462df1cddb275ac33d56a726b87
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-09-03 12:43:06 +0800

    sync

--- cluster/bash/search-PBC-darts-off.sh
+++ cluster/bash/search-PBC-darts-off.sh
@@ -33,8 +33,8 @@ echo Copying DONE.
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 
-python train_search.py --batch_size 4 --epochs 50 --save PBC-darts-50-off
-# python train_search.py --batch_size 4 --epochs 50 --is_parallel 1 --local_mount 0
+python train_search.py --batch_size 4 --epochs 50 --save PBC-darts-off
+# python train_search.py --batch_size 4 --epochs 50 --save FOO --local_mount 0
 
 
 
--- cluster/bash/search-PBC.sh
+++ cluster/bash/search-PBC.sh
@@ -33,7 +33,7 @@ echo Copying DONE.
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 
-python train_search_ts.py --batch_size 4 --epochs 50 --save PBC-darts-50 
+python train_search_ts.py --batch_size 4 --epochs 50 --save PBC-darts
 # python train_search_ts.py --batch_size 4 --epochs 50 --save FOO --local_mount 0
 
 
