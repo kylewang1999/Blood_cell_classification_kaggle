b907074cb00c0e265b7d804283859ec05f6d683e
kylewang1999 <kaiyuan.kylewang@gmail.com>
2021-09-12 14:49:26 +0800

    sync

--- cluster/bash/search-410-darts-off.sh
+++ cluster/bash/search-410-darts-off.sh
@@ -1,32 +1,19 @@
 # Set up Kaggle
 conda install pytorch==1.1.0 
-pip install kaggle
-mkdir  ~/.kaggle/
-cp /k5wang-volume/Blood_cell_classification_kaggle/kaggle/kaggle.json ~/.kaggle/kaggle.json
+# pip install kaggle
+# mkdir  ~/.kaggle/
+# cp /k5wang-volume/Blood_cell_classification_kaggle/kaggle/kaggle.json ~/.kaggle/kaggle.json
 
 echo Start Copying
 SECONDS=0
-# cp -r /k5wang-volume/Blood_cell_classification_kaggle/kaggle/BCCD_Reorganized /local/kaggle/BCCD_Reorganized
+
 mkdir /local/kaggle
 cd /local/kaggle
-kaggle datasets download -d kylewang1999/pbc-dataset
-unzip pbc-dataset.zip
+git clone https://github.com/kylewang1999/BCCD_Dataset.git
 pwd
 ls
 
 # Organize Data Dir
-org_dir () {
-    mv eosinophil 0_eosinophil
-    mv lymphocyte 1_lymphocyte
-    mv monocyte 2_monocyte
-    mv neutrophil 3_neutrophil
-}
-cd PBC_dataset_split/PBC_dataset_split/Train
-org_dir
-cd ../Test
-org_dir
-cd ../Val
-org_dir
 
 cd /k5wang-volume/Blood_cell_classification_kaggle/darts-LPT
 pwd
